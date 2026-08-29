812dd07a97233b7504119e6b9e6b4788fc3ed6bb
lenardmagat <lenardmagat76@gmail.com>
2026-08-29 22:04:37 +0800

    making MessageType can be json serialize

--- Datas/Models/ChatMessageModel.cs
+++ Datas/Models/ChatMessageModel.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 namespace ChatSystem.Models;
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum MessageType
 {
     Image,
