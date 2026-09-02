166a55e2fd0fa1517ad34dc4bc77cd7347c5fdf6
lenardmagat <lenardmagat76@gmail.com>
2026-09-02 22:55:41 +0800

    create another pipeline to check if the parentoffer is existing

---
+++ Services/EventsServices/IsOfferExisting.cs
@@ -0,0 +1,79 @@
+using ChatSystem.core;
+using ChatSystem.DataBase;
+using ChatSystem.DTOs;
+using ChatSystem.ErrorHandling;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatSystem.PipeLine.IsOfferExisting;
+public interface IOfferExist
+{
+    string ParentOfferId {get;}
+    OfferTye OfferType {get;}
+}
+public class OfferCheck<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IOfferExist
+    where TResponse : Result
+{
+    private readonly DbManager _db;
+    private readonly IHasher _hasher;
+    public OfferCheck(DbManager db, IHasher hasher)
+    {
+        _db = db;
+        _hasher = hasher;
+    }
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellation)
+    {
+        Result<int> Decoded;
+        if(request.OfferType == OfferTye.Sale)
+        {
+            Decoded = _hasher.DecodeOrFail(request.ParentOfferId, HashContext.SaleOffer);
+        }
+        else
+        {
+            Decoded = _hasher.DecodeOrFail(request.ParentOfferId, HashContext.TradeOffer);
+        }
+        if (!Decoded.IsSuccess)
+        {
+            return CreateFailureResponse(Decoded.Error!, Decoded.StatusCode);
+        }
+        if(request.OfferType == OfferTye.Sale)
+        {
+            var offer= await _db.SaleOffers
+                .AsNoTracking()
+                .Where(o => o.Id == Decoded.Value)
+                .FirstOrDefaultAsync(cancellation);
+            if(offer is null)
+            {
+                return CreateFailureResponse("The offer is not existing", StatusCodes.Status400BadRequest);
+            }
+           return await next();
+        }
+        else
+        {
+            var offer= await _db.TradeOffers
+                .AsNoTracking()
+                .Where(o => o.Id == Decoded.Value)
+                .FirstOrDefaultAsync(cancellation);
+            if(offer is null)
+            {
+                return CreateFailureResponse("The offer is not existing", StatusCodes.Status400BadRequest);
+            }
+           return await next();
+        }
+
+    }
+    private static TResponse CreateFailureResponse(string message, int statusCode)
+    {
+    if (typeof(TResponse).IsGenericType && typeof(TResponse).GetGenericTypeDefinition() == typeof(Result<>))
+    {
+        var resultType = typeof(TResponse).GetGenericArguments()[0];
+        var failureMethod = typeof(Result<>)
+            .MakeGenericType(resultType)
+            .GetMethod("Failure", new[] { typeof(string), typeof(int) });
+
+        return (TResponse)failureMethod!.Invoke(null, new object[] { message, statusCode })!;
+    }
+    return (TResponse)(object)Result.Failure(message, statusCode);
+    }
+}
