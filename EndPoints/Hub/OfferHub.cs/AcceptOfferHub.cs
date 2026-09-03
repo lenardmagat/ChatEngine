55c8e2c5a0f5fff1821819177ec24758e6f61335
lenardmagat <lenardmagat76@gmail.com>
2026-09-03 15:46:48 +0800

    add hub for AcceptOffer

--- EndPoints/Hub/OfferHub.cs/AcceptOfferHub.cs
+++ EndPoints/Hub/OfferHub.cs/AcceptOfferHub.cs
@@ -1,25 +1,25 @@
 using ChatSystem.DTOs;
 using ChatSystem.Extensions;
-using ChatSystem.SystemEvents.UnifiedProposedMechanism;
+using ChatSystem.SystemEvents.UnifiedAcceptMechanism;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatSystem.Hubs;
 public partial class AppHub
 {
-    public async Task ProposedOffer(ProposedItemDTO proposedItem)
+    public async Task AcceptOffer(AcceptItemDTO acceptItem)
     {
-            var userId = Context.User!.GetUserId()!.Value;
+        var UserId = Context.User!.GetUserId()!.Value;
         try
         {
-            UnifiedOffer.ProposedCommand proposedCommand = new UnifiedOffer.ProposedCommand
+            UnifiedAcceptOffer.AcceptOfferCommand command = new UnifiedAcceptOffer.AcceptOfferCommand
             {
-                UserId = userId,
-                ProposedData = proposedItem,
+                UserId = UserId,
+                itemDTO = acceptItem
             };
-            var result = await _mediator.Send(proposedCommand);
+            var result = await _mediator.Send(command);
             if (!result.IsSuccess)
             {
-                await Clients.Caller.SendAsync("ProposedOfferResponse", new
+                await Clients.Caller.SendAsync("MessageError",new
                 {
                     Details = result,
                     timestampt = DateTime.UtcNow
@@ -31,12 +31,11 @@ public partial class AppHub
                 await Clients.Caller.SendAsync("ProposedOfferResponse", result.Value);
                 await Clients.Groups($"UserNotification_{result.Value!.ReceipientId}").SendAsync("NewMessageNotification", result.Value);
                 await Clients.Groups($"Room_{result.Value!.RoomId}").SendAsync("NewMessage", result.Value);
-                _logger.LogInformation($"User {userId} Successfully Proposed an item. Detals :{proposedItem}. timestampt: {DateTime.UtcNow}");
-            }  
-        }
-        catch(Exception e)
+                _logger.LogInformation($"User {UserId} Successfully Accept an item. Detals :{acceptItem}. timestampt: {DateTime.UtcNow}");
+            } 
+        }catch(Exception e)
         {
-            _logger.LogError(e, $"An unexpected error occured while handling Proposed Offer endpoint, for user {userId}. Details: {proposedItem}. timestampt{DateTime.UtcNow}");
+             _logger.LogError(e, $"An unexpected error occured while handling Accept Offer endpoint, for user {UserId}. Details: {acceptItem}. timestampt{DateTime.UtcNow}");
             await Clients.Caller.SendAsync("RequestError", new {context =  "an Unexpected error occured in our server", statsCode = StatusCodes.Status500InternalServerError, timestampt = DateTime.UtcNow});
         }
     }
